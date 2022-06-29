6ac9447a4301585e3605bf34ea4d4c121b36e9fd
sauledezma <s.ledezmaut2019@gmail.com>
2022-06-28 20:43:36 -0600

    modificacion de detail, creacion de editar y botones

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,21 +1,77 @@
-
-import React,{ useEffect, useState } from 'react';
-import { Text, Button, SafeAreaView } from "react-native";
+import React,  { useEffect, useState } from 'react';
+import { View, StyleSheet, Text, Button, SafeAreaView,TextInput, Switch } from "react-native";
+import db from './../../src/firebase/config';
+import {ref, set} from "firebase/database";
 
 export default function SettingsScreen({ route, navigation }) {
 
    const { item } = route.params;
+   const [status, setStatus] = useState(false);
+   const [areaText, setAreaText] = useState("");
+   const [pinText, setPinText] = useState("");
+   const [statusText, setStatusText] = useState(false);
+
+   const toggleSwitch = () => setStatusText(!statusText);
+
+  useEffect(()=>{
+    setAreaText(item.area)
+    setPinText(item.pin)
+    setStatusText(item.status)
+  }, [item])
 
   return (
-
-    <SafeAreaView>
+    <SafeAreaView style={styles.listItem}>
+      <View style={{alignItems:"left",flex:1, justifyContent: 'space-between'}}>
+      
       <Text>Id: {item.id}</Text>
-      <Text>Area: {item.area}</Text>
-      <Text>Pin: {item.pin}</Text>
-      <Text>Estatus: {item.status ? "Encendido" : "Apagado"}</Text>
+      <Text>Area:</Text>
+      <TextInput onChangeText={setAreaText} value={areaText} editable={status ? true : false}/>
+      <Text>Pin:</Text>
+      <TextInput onChangeText={setPinText} value={pinText} editable={status ? true : false}/>
+      <Text>Estatus:</Text>
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={item.status ? "#f5dd4b" : "#f4f3f4"}
+        onValueChange={toggleSwitch}
+        disabled={status ? false : true}
+        value={statusText}
+      />
+
+      <Button onPress={() =>{
+        setStatus(!status);
+        if(status == true){
+          set(ref(db, 'devices/' + item.id), {
+                          area: areaText,
+                          status: statusText,
+                          pin : pinText
+                        });
+          setStatus(false)
+        }
+      }} title={status ? "Guardar" : "Editar"}/>
+
       <Button onPress={() =>{
         navigation.navigate('Home');
       }} title="REGRESAR" />
+      
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F7F7F7',
+    marginTop:60
+  },
+  listItem:{
+    margin:10,
+    padding:10,
+    backgroundColor:"#FFF",
+    width:"80%",
+    flex:1,
+    alignSelf:"center",
+    flexDirection:"row",
+    borderRadius:5
+  }
+});
