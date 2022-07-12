199e57f44e247957f5c57002ddfcf712a74f7286
sauledezma <s.ledezmaut2019@gmail.com>
2022-07-11 18:05:51 -0600

    Registrar

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
-import { FlatList, SafeAreaView,Button} from 'react-native';
+import {FlatList, SafeAreaView, Button} from 'react-native';
 import db from './../../src/firebase/config';
 import {ref, onValue} from "firebase/database";
 import Led from '../components/Led';
@@ -8,7 +8,6 @@ import Led from '../components/Led';
 export default function HomeScreen({ navigation }) {
 
   const [listDevices, setlistDevices] = useState([]);
-
 
   const  readData = () => {
     const dbRef = ref(db, 'devices');
@@ -27,16 +26,23 @@ export default function HomeScreen({ navigation }) {
 
   return (
     <SafeAreaView>
-       <Button onPress={() =>{
+      
+      <Button onPress={() =>{
         navigation.navigate('Create');
       }} title="Crear" />
+
       <FlatList
           data={listDevices}
           keyExtractor={(item, index) => String(index)}
           renderItem={({ item }) => (
-            <Led item={item} navigation={navigation}
-          />)}
+            <Led item={item} navigation={navigation}/>
+          )}
       />
+
+        <Button onPress={() =>{
+        navigation.navigate('Register');
+      }} title="Registrarse" />
+      
     </SafeAreaView>
   );
 }
